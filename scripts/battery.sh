d1b9696d5bdb289271a446769cbac16edba04fa9
Logan Hetzel <lhetzel@varmour.com>
2020-06-24 22:22:28 -0500

    lines?

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -102,9 +102,9 @@ airpods_status() {
     	declare -x "${info}"="$(awk -v pat="${info}" '$0~pat{gsub (";",""); print $3 }'<<<"${BT_DATA}")"
     	[[ ! -z "${!info}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15)": "}'<<<"${info}")${!info}%"
   	done
-   		printf "%s\\n" "${AIRPOD_ICON} ${OUTPUT}"
+   		printf "%s" "${AIRPOD_ICON} ${OUTPUT}"
 	else
-  		printf "%s Not Connected\\n" "${OUTPUT}"
+  		printf "%s Not Connected" "${OUTPUT}"
 	fi
 }
 
