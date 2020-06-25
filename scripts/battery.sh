134dd0bbebc0d8644723cc4f1bbd1d72959309f2
Logan Hetzel <lhetzel@varmour.com>
2020-06-24 22:23:38 -0500

    remove - we'll make airpods a different thing.

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -79,38 +79,8 @@ battery_status()
 	fi
 }
 
-airpods_status() {
-	AIRPOD_ICON=$'\uF7CC';
-
-	BATTERY_INFO=(
-  	"BatteryPercentCombined" 
-  	"HeadsetBattery" 
-  	"BatteryPercentSingle" 
-  	"BatteryPercentCase" 
-  	"BatteryPercentLeft" 
-  	"BatteryPercentRight"
-	)
-
-	BT_DEFAULTS=$(defaults read /Library/Preferences/com.apple.Bluetooth)
-	SYS_PROFILE=$(system_profiler SPBluetoothDataType 2>/dev/null)
-	MAC_ADDR=$(grep -b2 "Minor Type: Headphones"<<<"${SYS_PROFILE}"|awk '/Address/{print $3}')
-	CONNECTED=$(grep -ia6 "${MAC_ADDR}"<<<"${SYS_PROFILE}"|awk '/Connected: Yes/{print 1}')
-	BT_DATA=$(grep -ia6 '"'"${MAC_ADDR}"'"'<<<"${BT_DEFAULTS}")
-
-	if [[ "${CONNECTED}" ]]; then
-  	for info in "${BATTERY_INFO[@]}"; do
-    	declare -x "${info}"="$(awk -v pat="${info}" '$0~pat{gsub (";",""); print $3 }'<<<"${BT_DATA}")"
-    	[[ ! -z "${!info}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15)": "}'<<<"${info}")${!info}%"
-  	done
-   		printf "%s" "${AIRPOD_ICON} ${OUTPUT}"
-	else
-  		printf "%s Not Connected" "${OUTPUT}"
-	fi
-}
-
 main()
 {
-	airpods=$(airpods_status)
 	bat_stat=$(battery_status)
 	bat_perc=$(battery_percent)
 
@@ -119,9 +89,8 @@ main()
 	elif [ -z "$bat_perc" ]; then # In case it is a desktop with no battery percent, only AC power
 		echo "♥ $bat_stat"
 	else
-		echo "♥ $bat_stat $bat_perc "
+		echo "♥ $bat_stat $bat_perc"
 	fi
-	echo "$airpods"
 }
 
 #run main driver program
